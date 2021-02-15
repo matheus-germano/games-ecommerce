db11cee238343ec59a6b3c4195dbac50146348bc
Matheus Germano da Costa <matheus.ggcosta2014@gmail.com>
2021-02-15 13:00:38 -0300

    adding featureGames

--- src/components/Products/data.js
+++ src/components/Products/data.js
@@ -5,6 +5,30 @@ import forza4 from '../../images/forza4.png'
 import gears5 from '../../images/gears5.png'
 import sot from '../../images/sot.png'
 
+export const featureGames = [
+  {
+    name: 'The last of us Part II',
+    price: 'R$ 30.00',
+    img: tlou2,
+    alt: 'game',
+    button: 'Comprar agora',
+  },
+  {
+    name: 'Uncharted 4',
+    price: 'R$ 10.00',
+    img: uncharted4,
+    alt: 'game',
+    button: 'Comprar agora',
+  },
+  {
+    name: 'Horizon Zero Dawn',
+    price: 'R$ 15.00',
+    img: hzd,
+    alt: 'game',
+    button: 'Comprar agora',
+  },
+];
+
 export const sonyGames = [
   {
     name: 'The last of us Part II',
